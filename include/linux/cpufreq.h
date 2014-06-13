8c3a36176ab9680ebd7917497e2569b033fb28c4
yank555-lu <yank555.lu@gmail.com>
2014-06-13 08:52:39 +0200

    cpufreq: add yankactive governor
    Interactive governor with, fow now :
    
    - hispeed_freq defaulting to lower than policy_max (1.73GHz)
    - target_loads increased to 95
    - min_sampling time reduced to 20ms
    - above_hispeed_delay increased to 80ms
    - timer_rate increased to 20ms
    
    cpufreq: yankactive: Optimize sync_freq implementation
    
    Don't let frequency drop below sync_freq if either load or
    frequency among other cpus is above a certain threshold,
    not necessarily both.
    
    Credits : cyanogen
    
    Signed-off-by: tarun93 <tarunmyid@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    Signed-off-by: tarun93 <tarunmyid@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -439,6 +439,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
 #endif
 
 /*********************************************************************
